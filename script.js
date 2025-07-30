7ed997d264700150313b75d4dcdba361f5ae9850
Arthur José <arthur.silva.tec@despertar.org.br>
2025-07-30 14:27:53 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+/*let a = "teste"
+console.log("hello world")
+console.log(3 > 5)
+console.log(9 + 5)
+console.log(a)*/
+
+/*let nome = "Arthur"
+
+console.log(`Meu nome é ${nome} Xavier`)*/
+
+/*let meuNome = "Arthur"
+
+console.log('Meu nome é "Arthur" e estou aprendendo js')
+//DECLARAÇÃO DE VARIAVEL E INICIALIZAÇÃO
+let nome = "teste";
+//REATRIBUIÇÃO
+nome = "outro valor"
+
+const num1 = 8;
+const num2 = 5;
+
+const resultado = num1 = num2;
+console.log(resultado)*/
+
+const numero = prompt("Digite um numero")
+const numero1 = prompt("Digite outro")
+
+const num1 = Number(numero)
+const num2 = Number(numero1)
+
+const resultado = numero + numero1
+alert(resultado)
+
